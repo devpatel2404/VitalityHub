a9064fe7461f1cc8631eaf0bea26dec16794411d
Dev Patel <144943599+devpatel2404@users.noreply.github.com>
2024-05-27 16:53:01 -0400

    Fixing Home Page

--- app/components/Home/Footer.tsx
+++ app/components/Home/Footer.tsx
@@ -1,6 +1,7 @@
 "use client"
 import MediaQuery from "react-responsive";
 import Link from "next/link";
+import './Home.css'
 
 export default function Footer() {
 
@@ -17,176 +18,65 @@ export default function Footer() {
     }
 
     return (
-        <main className={"w-screen bg-blue-800 text-white"}>
+        <main className={"w-screen bg-blue-800 text-white Footer pb-4 pt-4"}>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
                   integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
                   crossOrigin="anonymous"/>
             <link rel="stylesheet"
                   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-            <MediaQuery minWidth={1200}>
-                <div className={"flex justify-center h-80"}>
-                    <div className={"flex justify-center"}>
-                        <h1 className={"text-3xl mt-16 mr-32"}>VitalityHub</h1>
-                        <div className={"flex"}>
-                            <div className={"mt-32"} style={{width: '400px'}}>
-                                <h2 className={"text-2xl"}>Learn More About Us @</h2>
-                                <div className={"mt-2"}>
-                                    <a href={"https://www.instagram.com/"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px', color: 'white', fontSize: '30px'}}>
-                                        <i className={"fa fa-instagram"}></i></a>
-                                    <a href={"https://www.facebook.com/"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px', color: 'white', fontSize: '30px'}}>
-                                        <i className={"fa fa-facebook"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                                    <a href={"https://twitter.com/?lang=en"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px', color: 'white', fontSize: '30px'}}>
-                                        <i className={"fa fa-twitter"}></i></a>
-                                    <a href={"https://www.linkedin.com/"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px'}}><i
-                                        className={"fa fa-linkedin"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                                    <a href={"https://www.twitch.tv/"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px'}}><i
-                                        className={"fa fa-youtube-play"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                                    <a href={"https://www.twitch.tv/"} target={"_blank"}
-                                       style={{display: 'inline', margin: '10px'}}><i
-                                        className={"fab fa-tiktok"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"flex"} style={{marginTop: 'auto', marginBottom: 'auto'}}>
-                        <div className={"mr-6"} style={{marginTop: 'auto', marginBottom: 'auto'}}>
-                            <h1 className={"text-2xl mr-4"}>Products</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Apps</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Foods</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Exercises</h1>
-                        </div>
-                        <div className={"mr-6"} style={{marginTop: 'auto', marginBottom: 'auto'}}>
-                            <h1 className={"text-2xl mr-4"}>Resources</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Blog</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Community</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Exercises</h1>
-                        </div>
-                        <div style={{marginTop: 'auto', marginBottom: 'auto'}}>
-                            <h1 className={"text-2xl mr-4"}>Customer Service</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>FAQ</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Contact Us</h1>
-                            <h1 className={"text-xl mt-1 mb-1"}>Live Chat</h1>
-                        </div>
+            <div className={"text-center"}>
+                <h1 className={"text-3xl"}>VitalityHub</h1>
+            </div>
+            <div className={"flex justify-center mb-4 mt-2 FooterLinks"}>
+                <div className={"FooterProducts"} style={{marginTop: 'auto', marginBottom: 'auto'}}>
+                    <h1 className={"FooterLinksTitle"}>Products</h1>
+                    <h1 className={"mt-1 mb-1 FooterLinksSub"}>Apps</h1>
+                    <h1 className={"mt-1 mb-1 FooterLinksSub"}>Foods</h1>
+                    <h1 className={"mt-1 FooterLinksSub"}>Exercises</h1>
+                </div>
+                <div className={"FooterResources"} style={{marginTop: 'auto', marginBottom: 'auto'}}>
+                    <h1 className={"FooterLinksTitle"}>Resources</h1>
+                    <h1 className={" mt-1 mb-1 FooterLinksSub"}>Blog</h1>
+                    <h1 className={"text-xl mt-1 mb-1 FooterLinksSub"}>Community</h1>
+                    <h1 className={"text-xl mt-1 FooterLinksSub"}>Exercises</h1>
+                </div>
+                <div className={"FooterCustomerService"}>
+                    <h1 className={"FooterLinksTitle"}>Customer Service</h1>
+                    <h1 className={"mt-1 mb-1 FooterLinksSub"}>FAQ</h1>
+                    <h1 className={"mt-1 mb-1 FooterLinksSub"}>Contact Us</h1>
+                    <h1 className={"mt-1 FooterLinksSub"}>Live Chat</h1>
+                </div>
+            </div>
+            <Link href={"/Get-Started"}>
+                <button id={"button"} onMouseEnter={Enter} onMouseLeave={Leave}
+                        className={"FooterButton flex text-lg text-blue-800 border-white border-2 rounded-3xl p-2 pl-6 pr-6 bg-white font-bold font-sans"}>
+                    START TODAY
+                </button>
+            </Link>
+            <div className={"flex justify-center mt-2"}>
+                <div className={"FooterSocials"}>
+                    <div className={"flex justify-evenly"}>
+                        <a href={"https://www.instagram.com/"} target={"_blank"}
+                           style={{display: 'inline', marginTop: 'auto', marginBottom: 'auto'}}>
+                            <i className={"fa fa-instagram"} style={{color: 'white', fontSize: '30px'}}></i></a>
+                        <a href={"https://www.facebook.com/"} target={"_blank"}
+                           style={{display: 'inline', marginTop: 'auto', marginBottom: 'auto'}}>
+                            <i className={"fa fa-facebook"} style={{color: 'white', fontSize: '30px'}}></i></a>
+                        <a href={"https://twitter.com/?lang=en"} target={"_blank"}
+                           style={{display: 'inline', color: 'white', fontSize: '30px'}}>
+                            <i className={"fa fa-twitter"} style={{color: 'white', fontSize: '30px'}}></i></a>
+                        <a href={"https://www.linkedin.com/"} target={"_blank"}
+                           style={{display: 'inline', marginTop: 'auto', marginBottom: 'auto'}}><i
+                            className={"fa fa-linkedin"} style={{color: 'white', fontSize: '30px'}}></i></a>
+                        <a href={"https://www.twitch.tv/"} target={"_blank"}
+                           style={{display: 'inline', marginTop: 'auto', marginBottom: 'auto'}}><i
+                            className={"fa fa-youtube-play"} style={{color: 'white', fontSize: '30px'}}></i></a>
+                        <a href={"https://www.twitch.tv/"} target={"_blank"}
+                           style={{display: 'inline', marginTop: 'auto', marginBottom: 'auto'}}><i
+                            className={"fab fa-tiktok"} style={{color: 'white', fontSize: '30px'}}></i></a>
                     </div>
                 </div>
-            </MediaQuery>
-            <MediaQuery maxWidth={599}>
-                <div className={"flex justify-center"}>
-                    <div>
-                        <h1 className={"text-4xl text-center mt-6"}>VitalityHub</h1>
-                        <div className={"flex justify-center mt-8"}>
-                            <div className={"mr-4 ml-4"}>
-                                <h1 className={"text-2xl mr-4"}>Products</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Apps</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Foods</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Exercises</h1>
-                            </div>
-                            <div className={"mr-4 ml-4"}>
-                                <h1 className={"text-2xl mr-4"}>Resources</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Blog</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Community</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Exercises</h1>
-                            </div>
-                            <div className={"mr-4 ml-4"}>
-                                <h1 className={"text-2xl mr-4"}>Customer Service</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>FAQ</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Contact Us</h1>
-                                <h1 className={"text-xl mt-1 mb-1"}>Live Chat</h1>
-                            </div>
-                        </div>
-                        <Link href={"/Get-Started"}>
-                            <button id={"button"} onMouseEnter={Enter} onMouseLeave={Leave}
-                                    className={"text-lg text-blue-800 border-white border-2 rounded-3xl p-2 pl-6 pr-6 bg-white font-bold font-sans mt-6 relative left-1/3"}>
-                                START TODAY
-                            </button>
-                        </Link>
-                        <div className={"flex justify-center mt-4"}>
-                            <a href={"https://www.instagram.com/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i className={"fa fa-instagram"} style={{
-                                color: 'white',
-                            fontSize: '30px'}}/></a>
-                            <a href={"https://www.facebook.com/"} target={"_blank"}
-                            style={{display: 'inline', margin: '10px'}}><i
-                            className={"fa fa-facebook"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                            <a href={"https://twitter.com/?lang=en"} target={"_blank"}
-                           style={{display: 'inline', margin: '10px'}}><i className={"fa fa-twitter"} style={{
-                            color: 'white',
-                            fontSize: '30px'}}/></a>
-                            <a href={"https://www.linkedin.com/"} target={"_blank"}
-                           style={{display: 'inline', margin: '10px'}}><i
-                            className={"fa fa-linkedin"} style={{color: 'white', fontSize: '30px'}} /></a>
-                            <a href={"https://www.twitch.tv/"} target={"_blank"}
-                           style={{display: 'inline', margin: '10px'}}><i
-                            className={"fa fa-youtube-play"} style={{color: 'white', fontSize: '30px'}} /></a>
-                            <a href={"https://www.twitch.tv/"} target={"_blank"}
-                           style={{display: 'inline', margin: '10px'}}><i
-                            className={"fab fa-tiktok"} style={{color: 'white', fontSize: '30px'}} /></a>
-                        </div>
-                    </div>
-                </div>
-            </MediaQuery>
-            <MediaQuery minWidth={600} maxWidth={1199}>
-                <div className={"flex justify-center"}>
-                    <div>
-                        <h1 className={"text-5xl text-center mt-6"}>VitalityHub</h1>
-                        <div className={"flex justify-center mt-8"}>
-                            <div className={"mr-4"}>
-                                <h1 className={"text-3xl"}>Products</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Apps</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Foods</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Exercises</h1>
-                            </div>
-                            <div className={"mr-4 ml-4"}>
-                                <h1 className={"text-3xl"}>Resources</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Blog</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Community</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Exercises</h1>
-                            </div>
-                            <div className={"ml-4"}>
-                                <h1 className={"text-3xl"}>Customer Service</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>FAQ</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Contact Us</h1>
-                                <h1 className={"text-2xl mt-1 mb-1"}>Live Chat</h1>
-                            </div>
-                        </div>
-                        <Link href={"/Get-Started"}>
-                            <button id={"button"} onMouseEnter={Enter} onMouseLeave={Leave}
-                                    className={"text-lg text-blue-800 border-white border-2 rounded-3xl p-2 pl-6 pr-6 bg-white font-bold font-sans mt-6 relative left-1/3"}>
-                                START TODAY
-                            </button>
-                        </Link>
-                        <div className={"flex justify-center mt-4"}>
-                            <a href={"https://www.instagram.com/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i className={"fa fa-instagram"} style={{
-                                color: 'white',
-                                fontSize: '30px'
-                            }}/></a>
-                            <a href={"https://www.facebook.com/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i
-                                className={"fa fa-facebook"} style={{color: 'white', fontSize: '30px'}}></i></a>
-                            <a href={"https://twitter.com/?lang=en"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i className={"fa fa-twitter"} style={{
-                                color: 'white',
-                                fontSize: '30px'
-                            }}/></a>
-                            <a href={"https://www.linkedin.com/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i
-                                className={"fa fa-linkedin"} style={{color: 'white', fontSize: '30px'}}/></a>
-                            <a href={"https://www.twitch.tv/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i
-                                className={"fa fa-youtube-play"} style={{color: 'white', fontSize: '30px'}}/></a>
-                            <a href={"https://www.twitch.tv/"} target={"_blank"}
-                               style={{display: 'inline', margin: '10px'}}><i
-                                className={"fab fa-tiktok"} style={{color: 'white', fontSize: '30px'}}/></a>
-                        </div>
-                    </div>
-                </div>
-            </MediaQuery>
+            </div>
         </main>
     );
 }
