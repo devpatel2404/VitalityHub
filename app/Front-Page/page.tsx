e9a1c9aaf0772de63610b7b8de03a1e81cb7947c
Dev Patel <144943599+devpatel2404@users.noreply.github.com>
2024-07-04 15:13:16 -0400

    Making Pages

--- app/Front-Page/page.tsx
+++ app/Front-Page/page.tsx
@@ -1,11 +1,4 @@
-
-const TokenManager = require('../TokenManager');
-
 export default function frontPage() {
-
-    const tokenManager = new TokenManager();
-    const Token = tokenManager.getToken();
-
     return (
         <main>
             <h1>Logged In Home Page</h1>
